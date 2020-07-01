22b459f2e79288288e7834462bab7baca31fa4d0
HomerJohnston <kyle.wilcox.unb@gmail.com>
2020-06-30 23:27:24 -0600

    Create discord_config.py

---
+++ discord_config.py
@@ -0,0 +1,27 @@
+import os
+# This file simply contains configurable data for use in other scripts.
+
+# Discord Webhook (personal server)
+TEST_WEBHOOK_URL = 'DISCORD_WEBHOOK_FROM_YOUR_TEST_CHANNEL'
+P4_COMMITS_WEBHOOK_URL = 'DISCORD_WEBHOOK_FROM_YOUR_ACTUAL_CHANNEL'
+
+# Perforce login info
+P4LOGIN = 'xxxxx'
+P4PASS = 'xxxxx'
+P4PORT = 'xxxxx'
+
+# Icons
+changelist_icon = 'https://i.imgur.com/TzA17kl.png'
+success_icon = 'https://i.imgur.com/xc3kKpX.png'
+error_icon = 'https://i.imgur.com/4vNtKWB.png'
+
+# Discord Users
+discord_userids = {
+        'P4USERNAME1' : 'DISCORDUSERIDNUMBER1',
+        'P4USERNAME2' : 'DISCORDUSERIDNUMBER'
+}
+
+discord_usernames = {
+        'P4USERNAME1' : 'DISCORDUSERNAME1',
+        'P4USERNAME1' : 'DISCORDUSERNAME1'
+}
